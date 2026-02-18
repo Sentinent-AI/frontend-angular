9e65d95762ff8eddb487af01bf50b1476f860055
siddy819 <60478931+siddy819@users.noreply.github.com>
2026-02-18 10:34:13 -0500

    feat(dashboard): implement dashboard (ref Sentinent-AI/Sentinent#8) (#3)

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,16 +1,29 @@
-import { Component, inject } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth';
+import { WorkspaceService } from '../../services/workspace';
+import { Workspace } from '../../models/workspace';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
-export class Dashboard {
+export class Dashboard implements OnInit {
   private authService = inject(AuthService);
+  private workspaceService = inject(WorkspaceService);
   private router = inject(Router);
+
+  workspaces: Workspace[] = [];
+
+  ngOnInit() {
+    this.workspaceService.getWorkspaces().subscribe(ws => {
+      this.workspaces = ws;
+    });
+  }
 
   logout() {
     this.authService.logout();
